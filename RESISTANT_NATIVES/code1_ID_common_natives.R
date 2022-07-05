390778e82b1e765c9689494ccc7b8425c6001a24
Fabiogeography <54939428+Fabiogeography@users.noreply.github.com>
2022-07-05 17:04:05 +0100

    Update 5th July 2022

--- RESISTANT_NATIVES/code1_ID_common_natives.R
+++ RESISTANT_NATIVES/code1_ID_common_natives.R
@@ -6,177 +6,118 @@
 ########################################################################
 
 library(dplyr)
+library(ggplot2)
+library(purrr) ## map_dfr
+library(tidyverse) ## group_split
 
 wd.dat <- "E:/NON_PROJECT/WORKSHOPS/POWELL/DATA/SPCIS/"
-wd.out <- "E:/NON_PROJECT/WORKSHOPS/POWELL/RESULTS/"
+wd.out <- "E:/NON_PROJECT/WORKSHOPS/POWELL/DATA/RESILIENT_NATIVES/"
 
-dat <- read.csv(paste0(wd.dat, "FULLDatabase_05272022.csv"))
+##### Read in species data and join environment data for each plot #####
+dat <- read.csv(paste0(wd.dat, "FULLDatabase_05272022.csv")) ## 1874220 rows
+dat$SpCode <- as.factor(dat$SpCode) ## easier to glance at data this way
+dat <- dat[!is.na(dat$SpCode),]
+length(unique(dat$SpCode)) ## 14162 species
 
-length(unique(dat$SpCode)) ## 14163 species
+## plots file contains the invader metrics for each plot. 
+## This includes plots with > 10% relative cover of unknown species - might want to remove later. 
+plots <- read.csv(paste0(wd.dat, "FULLDatabase_05272022_plotsenv4Jul2022.csv"))
+plots <- plots[,!names(plots) %in% c("Dataset", "Original.Plot","Site","Original.Site","FuzzedCoord","Zone","PlotArea.m2","SamplingMethod","Resampled","Long","Lat")] ## Remove names that are also in dat
 
-##### Identify commonest natives #####
+dat <- merge(plots, dat, by.x=c("Plot", "Year"), by.y=c("Plot", "Year")) ## merge based on plot and year. Plots that don't have coordinates are not retained. 1770529 rows
+
+##### Add invader metrics for each plot ######
+dat.I <- read.csv(paste0(wd.dat, "FullDatabase_diversity.csv"))
+dat.I <- dat.I[,!names(dat.I) %in% c("Dataset","Site","PlotArea.m2","Long","Lat")] ## Remove names that are also in dat. except Plot and Year, the join columns
+
+table(dat$Plot %in% dat.I$Plot) ## 143280 Plot IDs that are not in FullDatabase_diversity.csv, approx. 10% of all plots (1627249 that are)
+
+dat <- left_join(dat, dat.I, by = c("Plot", "Year")) ## Function used by Eve, rather than merge. Weird that gained 56 rows. 
+
+##### Identify commonest natives in the remaining plots #####
 frq <- table(dat$SpCode)
 
-cmn <- names(frq[frq > 400]) ## 941 species
-cmn <- cmn[cmn %in% dat$SpCode[dat$NativeStatus=="N"]] ## 827 of those species are native
-
-
-##### OLD #####
-frq <- table(dat$SpCode)
-frq <- frq[frq<1000] ## 367 species with an inordinately high number of plots - remove
-
-par(mfrow=c(2,2))
-hist(dat$PctCov[dat$PctCov<100], xlab="Pct cover < 100", main="")
-hist(frq, xlab="Num. plots < 1000", main="")
-boxplot(dat$PctCov ~ dat$NativeStatus)
-boxplot(log(dat$PctCov) ~ dat$NativeStatus)
-
-
-##### Identify rare and rarest natives #####
-### Rare species - need to be sufficiently widespread but locally rare 
-rare <- names(frq[frq > 150 & frq < 250]) ## 643 species are found in 150-250 plots
-rare <- rare[rare %in% dat$SpCode[dat$NativeStatus=="N"]] ## 583 of those species species are native
-
-## Calculate the mean and max coverage of the species in 150-250 plots
-rare <- dat[dat$SpCode %in% rare,] %>%
-  group_by(SpCode) %>%
-  summarise(
-    num_plots = n(),
-    mean_cov = mean(PctCov),
-    max_cov = max(PctCov)
-  )
-    
-par(mfrow=c(3,2))
-hist(rare$mean_cov, xlab="Mean PctCov", main="Species in 150-250 plots")
-hist(rare$max_cov, xlab="Max PctCov", main="Species in 150-250 plots")
-
-## Reduce the rare species list to those with <5% mean PctCov and <20% max PctCov
-rare <- rare[rare$mean_cov<5 & rare$max_cov<20,] ## 232 species
-hist(rare$mean_cov, xlab="Mean PctCov", main="Species in 150-250 plots\n<5% mean PctCov, <20% max PctCov")
-hist(rare$max_cov, xlab="Max PctCov", main="Species in 150-250 plots\n<5% mean PctCov, <20% max PctCov")
-
-## Reduce the rare species list to those with <1% mean PctCov and <5% max PctCov
-rarest <- rare[rare$mean_cov<1 & rare$max_cov<5,] ## 59 species
-hist(rarest$mean_cov, xlab="Mean PctCov", main="Species in 150-250 plots\n<1% mean PctCov, <5% max PctCov")
-hist(rarest$max_cov, xlab="Max PctCov", main="Species in 150-250 plots\n<1% mean PctCov, <5% max PctCov")
-
-jpeg(paste0(wd.out, "PctCov_rarest.jpg"))
-par(mfrow=c(1,2))
-hist(rarest$mean_cov, xlab="Mean PctCov", main="Species in 150-250 plots\n<1% mean PctCov, <5% max PctCov")
-hist(rarest$max_cov, xlab="Max PctCov", main="Species in 150-250 plots\n<1% mean PctCov, <5% max PctCov")
-dev.off()
-
-## Save the data for the rare and rarest species
-dat.rare <- dat[dat$SpCode %in% rare$SpCode,]
-# write.csv(dat.rare, paste0(wd.out, "dat_rare.csv"), row.names=F)
-
-dat.rarest <- dat[dat$SpCode %in% rarest$SpCode,]
-# write.csv(dat.rarest, paste0(wd.out, "dat_rarest.csv"), row.names=F)
-
-##### ID the non-natives that co-occur with the rare and rarest natives #####
-## Non-natives that co-occur at least once with the rarest natives
-plots.rarest <- dat$Plot[dat$Plot %in% dat.rarest$Plot] ## Plots that contain the rarest species
-IAS.rarest <- unique(na.omit(dat[dat$Plot %in% plots.rarest & dat$NativeStatus=="I",c("Plot","SpCode")]))
-length(unique(IAS.rarest$SpCode)) ## 583 co-occurring non-natives
-
-## Non-natives that co-occur in at least 50 plots with the rarest natives
-# test <- dat[dat$SpCode %in% rarest$SpCode ... dat$SpCode %in% IAS.rarest$SpCode]
-IAS.ntv.rarest <- unique(merge(IAS.rarest, dat.rarest[,c("Plot","SpCode")], by="Plot")) ## 19469 plots that contain one of the rarest and one or more non-native species
-colnames(IAS.ntv.rarest)[2:3] <- c("IAS","ntv")
-
-IAS.ntv.rarest <- IAS.ntv.rarest %>%
-  group_by(IAS, ntv) %>%
-  summarise(
-    num_plots = n(),
-  )
-
-hist(IAS.ntv.rarest$num_plots)
-table(IAS.ntv.rarest$num_plots)
-sum(table(IAS.ntv.rarest$num_plots[IAS.ntv.rarest$num_plots > 50])) ## 63 IAS:ntv species combinations where both appear in at least 50 plots together
-     
-IAS.ntv.rarest <- IAS.ntv.rarest[IAS.ntv.rarest$num_plots > 50,]
+length(cmn <- names(frq[frq > 400])) ## 914 species found in > 400 plots of the surviving dataset
+length(cmn <- cmn[cmn %in% dat$SpCode[dat$NativeStatus=="N"]]) ## 801 of those species are native
 
 ## Calculate the mean and max coverage of the co-ocurring non-native species
-IAS.rarest <- dat[dat$SpCode %in% IAS.ntv.rarest$IAS,] %>% ## 23 co-occurring non-native species
-  group_by(SpCode) %>%
+length(dat[is.na(dat$NA_L1NAME),]) ## 73 points fall outside any ecoregion
+dat <- dat[!is.na(dat$NA_L1NAME),] 
+
+dat.smry <- dat[dat$SpCode %in% cmn,] %>% 
+  group_by(SpCode, NA_L1NAME) %>%
   summarise(
     num_plots = n(),
-    mean_cov = mean(PctCov),
-    max_cov = max(PctCov)
+    mean_cov = mean(PctCov_100),
+    max_cov = max(PctCov_100)
   )
 
-hist(IAS.rarest$num_plots, xlab="Num plots", main="Non-natives co-occurring with rarest natives")
-IAS.names <- unique(IAS.rarest$SpCode[IAS.rarest$num_plots>1000]) ## 9 co-occuring non-natives in > 1000 plots
+## Pick out the native species with > 400 records in **any given ecoregion**, not overall.
+dim(dat.smry.400 <- dat.smry[dat.smry$num_plots>400,]) ## 753 species * ecoregion combinations where the species is present in > 400 plots
+length(cmn.nms <- unique(dat.smry.400$SpCode)) ## 675 species are identified as common in at least one ecoregion
 
-par(mfrow=c(2,2))
-hist(IAS.rarest$mean_cov, xlab="Mean PctCov", main="Non-natives co-occurring with rarest\nnatives")
-hist(IAS.rarest$max_cov, xlab="Max PctCov", main="Non-natives co-occurring with rarest\nnatives")
+dat.cmn <- dat[dat$SpCode %in% cmn.nms,] ## Note that some of these native species are recorded as NI in some of the plots. 
 
-hist(IAS.rarest$mean_cov[IAS.rarest$num_plots>1000], xlab="Mean PctCov", main="Non-natives co-occurring with rarest\nnatives & in >1000 plots")
-hist(IAS.rarest$max_cov[IAS.rarest$num_plots>1000], xlab="Max PctCov", main="Non-natives co-occurring with rarest\nnatives & in >1000 plots")
+## Exclude plots whose RelCov_I was <2% or that are uninvaded, to avoid the "trace" cover artifact.
+## Don't do this now as it removes presences that are needed for the presence absence data frame
+# dat <- dat[dat$RelCov_I>=2  & !is.na(dat$RelCov_I),] ## Lose over 2/3rds of plots. This seems high - checking with Eve.
 
-jpeg(paste0(wd.out, "PctCov_non-ntv.jpg"))
-par(mfrow=c(1,2))
-hist(IAS.rarest$mean_cov[IAS.rarest$num_plots>1000], xlab="Mean PctCov", main="Non-natives co-occurring with rarest\nnatives & in >1000 plots")
-hist(IAS.rarest$max_cov[IAS.rarest$num_plots>1000], xlab="Max PctCov", main="Non-natives co-occurring with rarest\nnatives & in >1000 plots")
-dev.off()
+## Save the file that contains the cover of the common natives in each plot
+write.csv(dat.cmn, paste0(wd.out, "FULLDatabase_05272022_commonCov_env_IAS.csv"), row.names=F)
 
-## Isolate the plots for the 9 IAS that co-occur with the rarest natives
-dat.IAS.rarest <- dat[dat$SpCode %in% IAS.names,]
+##### Build dataframes that include presences and absences as well as abundances for each common species #####
+## For each species pull out the plots it is found and some plots in the level 4 ecoregion where it is absent 
+## The number of absences in each L4 ecoregion matches the number of presences, i.e. prevalence in each L4 ecoregion is 0.5
+## Alternative is to select absences from within a geographic or environmental buffer, but compare_env_ecoregions.R Found that L4 ecoregions explains the vast majority of variation in aridity, npp, human modification and min temp.
+##  Level IV ecoregions are intended for large geographic extents (i.e. states, multiple counties, or river basin: https://bit.ly/3yDxR9s
+## There are 967 ecoregions in the conterminous U.S.
 
-##### Find the commonest species that co-occur in the plots selected for the rarest species #####
-## Natives that co-occur at least once with the rarest natives
-common.rarest <- unique(na.omit(dat[dat$Plot %in% plots.rarest & dat$NativeStatus=="N",c("Plot","SpCode","PctCov")]))
-common.rarest <- common.rarest[!(common.rarest$SpCode %in% rarest$SpCode),] ## remove the rarest species
-length(unique(common.rarest$SpCode)) ## 7496 co-occurring natives
-hist(common.rarest$PctCov)
+set.seed(0) ## So that the absence plots sampled will be the same each time. Remove if you want to make multiple different samples. 
 
-### Select the 59 natives with the highest mean abundance in the plots containing the rarest species
-## Calculate the mean and max coverage of the co-occurring native species
-common.rarest <- common.rarest %>%
-  group_by(SpCode) %>%
-  summarise(
-    num_plots = n(),
-    mean_cov = mean(PctCov),
-    max_cov = max(PctCov)
-  ) %>%
-arrange(desc(mean_cov), .by_group = TRUE) ## Sort by the mean abundance
-        
-jpeg(paste0(wd.out, "PctCov_commonest.jpeg"))
-par(mfrow=c(1,2))
-hist(common.rarest$mean_cov, xlab="Mean PctCov", main="Natives co-occurring with rarest\nnatives")
-hist(common.rarest$max_cov, xlab="Max PctCov", main="Natives co-occurring with rarest\nnatives")
-dev.off()
+# sp <- "ACNE2"
+for (sp in cmn.nms[543:length(cmn.nms)]) {
 
-common.names <- common.rarest$SpCode[1:59] ## Select the 59 species with the highest mean abundance
+  ## Make the presence dataframe
+  pres <- dat.cmn[dat.cmn$SpCode==sp,]
+  pres$occ <- 1
+  
+  ## Get number of plots in each L4 ecoregion where the species is found
+  l4.count <- as.data.frame(table(dat.cmn[dat.cmn$SpCode==sp,"US_L4NAME"])) 
+  colnames(l4.count) <- c("US_L4NAME", "num_pres_plots")
+  
+  ## Select the same number of absences from each L4 ecoregion. This keeps the prevalence at 50% within each ecoregion.
+  abs <- dat[dat$SpCode!=sp,] ## Plots where the species is absent
+  abs <- left_join(abs, l4.count, by="US_L4NAME") ## Join the number of plots in each L4 ecoregion where the sepcies is found
+  abs <- abs[!is.na(abs$num_pres_plots),]
+  abs$occ <- 0
+  
+  abs <- abs %>% 
+    group_split(US_L4NAME) %>% ## Returns a list of tibbles. Each tibble contains the rows of .tbl for the associated group and all the columns, including the grouping variables.
+    map_dfr(~{ ## Apply a function to each element
+      sample_n(.x, num_pres_plots[1], replace = F) ## sample absence plots from each L4 ecoregion in proportion to the presence plots in that L4 ecoregion.
+      ## Sampling without replacement should be fine as there should be more plots where a given species is absent than present
+    })
+      
+  ## Checking the numbers are correct
+  print(paste(sp, sum(l4.count$num_pres_plots) == nrow(abs))) ## Ensure the presence plots in total matches the number absence plots selected
+  
+  abs$num_pres_plots <- NULL ## Remove the column indicating how many absences to select
+  
+  assign(paste0(sp, "_occ"), rbind(pres, abs))
+  
+  write.csv(get(paste0(sp, "_occ")), paste0(wd.out, sp, "_occ.csv"), row.names=F)
 
-##### Create data #####
-sp.vars <- c("SpCode","PctCov","RelCov", "Duration","Growth.Habit", "Woodiness") ## Variables we want for each species
-plot.vars <- c("Long", "Lat", "Dataset", "Site", "Ecoregion","PlotArea.m2","SamplingMethod") ## Variables we want for each plot
+}
 
-## Isolate the plots for the 9 IAS that co-occur with the rarest natives
-# dat.IAS.rarest <- dat[dat$SpCode %in% IAS.names,] ## already done above
-dat.IAS.ntv.rarest <- merge(dat.IAS.rarest[,c("Plot",sp.vars, plot.vars)], dat.rarest[,c("Plot",sp.vars, plot.vars)], by=c("Plot", plot.vars), all=T) ## 42193 plots. Removing all=T shows that 5207 plots that contain one of the rarest and one or more co-occurring non-native species
+## Failed at SAVE4. Probably need to set replace=T
 
-colnames(dat.IAS.ntv.rarest) <- gsub(".x", ".I", colnames(dat.IAS.ntv.rarest))
-colnames(dat.IAS.ntv.rarest) <- gsub(".y", ".NR", colnames(dat.IAS.ntv.rarest))
+##### Explore with plotting #####
+#Eastern Forests ecoregion only
+EForNat <- filter (dat.cmn.I, NA_L1NAME == "NORTH AMERICAN DESERTS")
+RandomSpp <- EForNat %>% filter (SpCode %in% sample (cmn.nms, 12))
+RandomSpp %>%
+  ggplot (aes(RelCov_I, PctCov_100)) + geom_point() +
+  geom_smooth(method='lm', formula= y~x) + facet_wrap(~SpCode, scales = "free_y")
 
-## Isolate the plots for the 59 common natives that co-occur with the rarest natives
-sp.vars.I <- paste0(c("SpCode","PctCov","RelCov", "Duration","Growth.Habit", "Woodiness"),".I") ## Variables we want for each species
-sp.vars.NR <- paste0(c("SpCode","PctCov","RelCov", "Duration","Growth.Habit", "Woodiness"),".NR") ## Variables we want for each species
-sp.vars.INR <- c(sp.vars.I, sp.vars.NR)
-
-dat.common.rarest <- dat[dat$SpCode %in% common.names,] ## Not very many rows - the very locally common species are not very widespread!
-dat.IAS.common.ntv.rarest <- merge(dat.IAS.ntv.rarest[,c("Plot",sp.vars.INR, plot.vars)], dat.common.rarest[,c("Plot",sp.vars, plot.vars)], by=c("Plot", plot.vars), all=T) ## 55781 plots. Removing all=T shows that ... plots that contain one of the rarest and one or more co-occurring non-native species
-colnames(dat.IAS.common.ntv.rarest)[21:26] <- paste0(colnames(dat.IAS.common.ntv.rarest[21:26]), ".NC")
-
-## Inspect
-par(mfrow=c(3,1))
-hist(dat.IAS.common.ntv.rarest$PctCov.NR)
-hist(dat.IAS.common.ntv.rarest$PctCov.NC)
-hist(dat.IAS.common.ntv.rarest$PctCov.I)
-
-## Save
-write.csv(dat.IAS.common.ntv.rarest, paste0(wd.out, "IAS_common_rarest_24May2022.csv"), row.names=F)
 
 
